0adfbaa85b50153b194b9011a40fd2c9bb286ae5
meghrajthakre <meghrajthakre444@gmail.com>
2026-03-30 19:06:13 +0530

    change the url

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://firstfreelance-project.vercel.app",
-  "https://firstfreelance-project-b9qx.vercel.app"
+  "https://superadmin-phi-eight.vercel.app"
 
 ];
 app.use(cors({
