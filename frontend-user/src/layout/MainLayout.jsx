029c895dc7a73b19c62cddd9db5b964a683c0d32
meghrajthakre <meghrajthakre444@gmail.com>
2026-04-07 17:18:19 +0530

    improve the layout of id

--- frontend-user/src/layout/MainLayout.jsx
+++ frontend-user/src/layout/MainLayout.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-import Navbar from '../components/Navbar'
-import MarqueeBanner from '../components/MarqueeBanner'
-import Footer from '../pages/Footer'
+import Navbar from "../components/Navbar";
+import MarqueeBanner from "../components/MarqueeBanner";
+import Footer from "../pages/Footer";
 
 const MainLayout = () => {
-
   return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
 
-    <div>
-      
+      {/* Fixed Top Section */}
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <Navbar />
+        <MarqueeBanner />
+      </div>
 
-      <Navbar />
-      <MarqueeBanner />
-      <Outlet />
-      <Footer />
+      {/* Main Content */}
+      <main className="flex-1 pt-[100px] px-3 sm:px-4 md:px-6">
+        <Outlet />
+      </main>
 
+      {/* Footer */}
+      <footer className="mt-auto">
+        <Footer />
+      </footer>
 
     </div>
+  );
+};
 
-  )
-
-}
-
-export default MainLayout
+export default MainLayout;
