400a4ef88013b8e20b3eb8de9b024d61b368235e
meghrajthakre <meghrajthakre444@gmail.com>
2026-04-08 16:10:33 +0530

    change the marqee baneer layout

--- frontend-user/src/layout/MainLayout.jsx
+++ frontend-user/src/layout/MainLayout.jsx
@@ -9,20 +9,27 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-dvh bg-gray-50">
 
-      {/* Fixed header: Navbar + Banner stacked naturally */}
+      {/* Header (Fixed like DHV) */}
       <header className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
-        <MarqueeBanner />
+
+
       </header>
 
-      {/* pt-24 = navbar (64px) + banner (32px) */}
-      <main className="pt-24">
+      {/* Content */}
+      <main
+      >
+
         <Outlet />
       </main>
 
+      {/* Footer */}
       <footer className="mt-auto">
+        <MarqueeBanner />
+
         <Footer />
       </footer>
+      
 
     </div>
   );
