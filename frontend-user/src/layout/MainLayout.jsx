4891480d038e570ddaf550a6daf62a775f2f655e
meghrajthakre <meghrajthakre444@gmail.com>
2026-04-08 16:06:55 +0530

    fix(layout): move banner inside fixed header, fix mobile visibility

--- frontend-user/src/layout/MainLayout.jsx
+++ frontend-user/src/layout/MainLayout.jsx
@@ -9,26 +9,20 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-dvh bg-gray-50">
 
-      {/* Header (Fixed like DHV) */}
+      {/* Fixed header: Navbar + Banner stacked naturally */}
       <header className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
         <MarqueeBanner />
-
-
       </header>
 
-      {/* Content */}
-      <main
-      >
-
+      {/* pt-24 = navbar (64px) + banner (32px) */}
+      <main className="pt-24">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <footer className="mt-auto">
         <Footer />
       </footer>
-      
 
     </div>
   );
