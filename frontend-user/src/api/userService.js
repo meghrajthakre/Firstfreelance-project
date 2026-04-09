75f86837150f553e6cfc6d9bf767662c26d346de
meghrajthakre <meghrajthakre444@gmail.com>
2026-04-09 15:56:27 +0530

    fix double .data unwrap on mobile

--- frontend-user/src/api/userService.js
+++ frontend-user/src/api/userService.js
@@ -9,5 +9,5 @@ export const logoutUser = () =>
 export const getMe = () =>
   API.get("/auth/me").then((r) => r.data);
 
-export const getBanner    = () => API.get("/banner");
+export const getBanner    = () => API.get("/banner").then((r) => r.data);
 export const updateBanner = (text) => API.put("/banner", { text }).then((r) => r.data);
